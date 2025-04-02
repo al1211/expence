077a2faba39f06b65e074341f02aef06cee7e1b8
al1211 <kanil013623@gmail.com>
2025-04-02 16:45:36 +0530

    create dashboard page layout

--- src/pages/Dashboard/Home.jsx
+++ src/pages/Dashboard/Home.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DashboradeLayout from '../../components/Layout/DashboradeLayout'
+import { useUserAuth } from '../../hooks/useUserAuth';
+import { useNavigate } from 'react-router-dom';
+import { API_PATH } from '../../utils/apipath';
+import axiosInstance from '../../utils/axiosInstance';
 
 const Home = () => {
+  useUserAuth();
+  const navigate=useNavigate();
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchDashboardData = async () => {
+    if(loading) return;
+    setLoading(true);
+
+    try{
+  
+      const response = await axiosInstance.get(`${API_PATH.DASHBOARD.GET_DATA}`);
+
+      if(response.data){
+        setDashboardData(response.data);
+      }
+    }catch(err){
+      console.error("Something went wrong. please try agin later:", err);
+    }finally{
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchDashboardData();
+    return ()=>{}
+  }, []);
   return (
-    <div>Home</div>
+    <DashboradeLayout activeMenu="Dashboard">
+      <div className='my-5 mx-auto'>Home</div>
+    </DashboradeLayout>
+ 
   )
 }
 
